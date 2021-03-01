51011a4ce1aa11612c3d9b9b84f5a17ae594b451
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2021-02-28 19:22:58 -0500

    Update serial_read.py

--- serial_read.py
+++ serial_read.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
             elif packetIndex> packetLength:
                checksum = latestByte
                
-               checksumAccumulator = 255 - checksumAccumulator
+               checksumAccumulator = np.uint8(255 - checksumAccumulator)
                print(checksum, checksumAccumulator)
                if checksum == checksumAccumulator:
                   parseSuccess, parsedData = parsePacket()
